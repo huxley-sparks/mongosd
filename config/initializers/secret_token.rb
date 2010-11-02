728143adbe8507cd52d2d8641b65ff882009cdc7
Huxley <huxley.spark@gmail.com>
2010-11-01 23:45:11 -0700

    Mongoid needs mongodb 1.6

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mongosd::Application.config.secret_token = 'f535c36cbfbd9abb3b4fdf3b5b99cab94e4a91b86d39ef506469ba10ca6b906b8f837f0efb63e5a6d2877a10d44475ec9e7db7a296d9a15d618c4fc8c484211a'
